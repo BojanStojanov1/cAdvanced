3e21abb86eaf676a020c32d64aa039f99d10124c
BojanStojanov1 <bojan.20406@gmail.com>
2025-05-10 15:38:32 +0200

    Delete Task4 directory

--- Task4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Task4.Models;
-
-Employee manager = new Manager("Bojan", 5000);
-Employee programmer = new Programmer("Vlatko", 50, 160);
-manager.DisplayInfo();
-programmer.DisplayInfo();
-
-Console.ReadLine();
-
