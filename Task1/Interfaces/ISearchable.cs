e45fd617617abfdc470c36cfe791717b6d0dd375
BojanStojanov1 <bojan.20406@gmail.com>
2025-05-10 15:37:49 +0200

    Delete Task1 directory

--- Task1/Interfaces/ISearchable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Task1.Interface
-{
-	public interface ISearchable
-	{
-        bool Search(string word);
-    }
-}
-
--- Task1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Task1.Interface;
-using Task1.Models;
-
-ISearchable doc = new Document("Hello world in a document.");
-ISearchable web = new WebPage("<html>Hello web page!</html>");
-Console.WriteLine(doc.Search("world"));
-Console.WriteLine(web.Search("page"));
-
